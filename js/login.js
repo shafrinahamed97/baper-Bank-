c13b97a86ac915269e0eb269117491b871f1ab5d
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-19 23:58:35 +0600

    Added bank site

--- js/login.js
+++ js/login.js
@@ -19,11 +19,11 @@ document.getElementById('btn-submit').addEventListener('click', function(){
 
     //Step-4: verify email and password and check whether valid user or not
     if(email === 'sontan@baap.com' && password === 'secret'){
-        console.log('valid user')
+        window.location.href = 'bank.html';
 
     }
     else{
-        console.log('invalid user');
+        alert('Toke teijjo gosona korlam, tue password vule gechoc')
     }
 
    
